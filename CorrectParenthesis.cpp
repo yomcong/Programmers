e07afb2c6ddc262c1d799720c188be9c66ab4c78
yomcong <89593036+yomcong@users.noreply.github.com>
2021-10-26 23:24:00 +0900

    Create CorrectParenthesis.cpp
    프로그래머스 올바른괄호 정답 소스

---
+++ CorrectParenthesis.cpp
@@ -0,0 +1,30 @@
+﻿#include<string>
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> stk;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            stk.push('(');
+        }
+        else
+        {
+            if (false==stk.empty() && stk.top() == '(')
+                stk.pop();
+            else
+                stk.push(')');
+        }
+    }
+    if (stk.empty())
+        return true;
+    else
+        return false;
+}
