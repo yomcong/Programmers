4299ba40aa38422145a31c5e27ca00f5134584e6
yomcong <89593036+yomcong@users.noreply.github.com>
2021-10-27 00:58:39 +0900

    Create Average.cpp
    프로그래머스 1단계 평균 구하기

---
+++ Average.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double answer = 0;
+	vector<int> arr;
+	arr.push_back(5);
+	arr.push_back(11);
+	arr.push_back(165);
+	arr.push_back(1);
+	arr.push_back(51);
+	arr.push_back(61);
+	for (int i = 0; i < arr.size(); ++i)
+	{
+		answer += arr[i];
+	}
+	answer /= arr.size();
+
+	cout << answer << endl;
+}
