3c61424cc304e837106692cb6e8c6e7e0ca90cbc
yomcong <89593036+yomcong@users.noreply.github.com>
2021-10-28 01:36:41 +0900

    Create EvenAndOdd.cpp
    프로그래머스 1단계 짝수와 홀수

---
+++ EvenAndOdd.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    string solution(int num)
+    {
+        string answer = "";
+        if (num % 2 == 0)
+            answer = "Even";
+        else
+            answer = "Odd";
+
+        return answer;
+    }
+
+
+
+}
