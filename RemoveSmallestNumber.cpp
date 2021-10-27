73142f9af5b6f4a64200d7df9a342621ec2279f6
yomcong <89593036+yomcong@users.noreply.github.com>
2021-10-28 01:37:18 +0900

    Create RemoveSmallestNumber.cpp
    프로그래머스 1단계 제일 작은 수 제거하기

---
+++ RemoveSmallestNumber.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 121;
+	int i = 1;
+
+	while (true)
+	{
+		if (i * i == n)
+			break;
+		else
+		{
+			i *= i;
+		}
+	}
+	i++;
+	i *= i;
+
+	cout << i << endl;
+	
+
+}
