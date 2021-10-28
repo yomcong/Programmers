eb938914527ee0a1fe2c3e3723b8ffd98cbccadb
yomcong <89593036+yomcong@users.noreply.github.com>
2021-10-29 01:03:09 +0900

    Create Descending.cpp
    정수 내림차순으로 배치하기

---
+++ Descending.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+
+using namespace std;
+
+bool desc(int a, int b)
+{
+	return a > b;
+}
+
+
+int main()
+{
+    long long solution(long long n) {
+        long long answer = 0;
+        string a = " ";
+        a += to_string(n);
+        sort(a.begin(), a.end(), desc);
+        answer = stoll(a);
+
+        return answer;
+}
