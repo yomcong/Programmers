f14dceb416b0966e9231d989f87a102ddcdc7ac0
yomcong <89593036+yomcong@users.noreply.github.com>
2021-10-30 19:47:35 +0900

    Create IntegerSquareRoot.cpp
    프로그래머스 1단계 정수 제곱근

---
+++ IntegerSquareRoot.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 3;
+	int i = 1;
+
+	while (true)
+	{
+		if (i * i == n)
+			break;
+		else if (i > n)
+		{
+			i = -1;
+			break;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	if (i <= 0)
+	{
+		cout << i << endl;
+	}
+	else
+	{
+		i++;
+		i *= i;
+		cout << i << endl;
+	}
+
+
+
+}
