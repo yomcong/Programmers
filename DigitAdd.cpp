f1ecb07de328470edbff485eb387f730b738c77f
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-01 00:57:31 +0900

    Create DigitAdd.cpp
    프로그래머스 1단계 자릿수 더하기

---
+++ DigitAdd.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+
+using namespace std;
+// 프로그래머스 1단계 자릿수 더하기
+
+int main()
+{
+	int answer = 0;
+	int a = 987;
+
+	do
+	{
+		answer += (a % 10);
+		a /= 10;
+	} while (a != 0);
+
+	cout << answer << endl;
+	
+}
