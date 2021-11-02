47523b0d2a62c041f0ffce6e65a621a070957a75
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-03 00:20:11 +0900

    Create Sumfactors.cpp
    프로그래머스 1단계 약수의 합

---
+++ Sumfactors.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+
+using namespace std;
+// 프로그래머스 1단계 약수의 합
+
+int main()
+{
+	int a = 12;
+	int temp = 0;
+
+	for (int i = 1; i <= a; i++)
+	{
+		if (a % i == 0)
+		{
+			temp += i;
+		}
+	}
+
+	cout << temp << endl;
+
+	// 
+	// 
+}
