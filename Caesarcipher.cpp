8f11abd9b85aee614b66a81b9d8f2de54a49c14b
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-04 00:46:26 +0900

    Create Caesarcipher.cpp
    프로그래머스 1단계 시저 암호

---
+++ Caesarcipher.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+
+using namespace std;
+// 프로그래머스 1단계 시저 암호
+
+int main()
+{
+	string s = "a B z";
+	int n = 25;
+	string temp="";
+
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if (s[i] == ' ')
+		{
+			temp += ' ';
+			continue;
+		}
+		if ((s[i] <= 90 && s[i] + n > 90) ||(s[i] >= 97 && s[i] + n > 122))
+		{
+			temp += s[i] - (26 - n);
+		}
+		else
+		{
+			temp += s[i] + n;
+		}
+		
+	}
+
+	cout << temp << endl;
+
+
+
+	// 
+	// 
+}
