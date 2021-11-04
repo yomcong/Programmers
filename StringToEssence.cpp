6d0da60466ee99bf80636204934ca1c1802bbc71
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-05 00:52:10 +0900

    Create StringToEssence.cpp
    프로그래머스 1단계 문자열을 정수로 바꾸기

---
+++ StringToEssence.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+
+using namespace std;
+// 프로그래머스 1단계 문자열을 정수로 바꾸기
+
+int main()
+{
+	string s = "+1234";
+	int answer = 0;
+
+	s[0] == '-' ? answer = stoi(s) : answer = stoi(s);
+	
+	cout << answer << endl;
+	// 
+	// 
+}
