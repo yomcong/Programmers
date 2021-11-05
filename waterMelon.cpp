0e36c5d541f0fd34c67dadb20102a3bfc1c7d36e
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-06 01:21:05 +0900

    Create waterMelon.cpp
    프로그래머스 1단계 수박수박

---
+++ waterMelon.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+
+using namespace std;
+// 프로그래머스 1단계 수박수박수박?
+
+int main()
+{
+	int n = 4;
+	string answer = "";
+
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			answer += "수";
+		}
+		else
+		{
+			answer += "박";
+		}
+	}
+
+	cout << answer << endl;
+	// 
+	// 
+}
