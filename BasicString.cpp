5e5f1ce9e96de6ceaaf333e25ac94be7464820e0
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-08 23:45:45 +0900

    Create BasicString.cpp
    프로그래머스 1단계 문자열 다루기 기본

---
+++ BasicString.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 프로그래머스 1단계 문자열 다루기 기본
+
+int main()
+{
+	string s = "1234";
+	bool answer = false;
+
+	if (s.size() == 4 || s.size() == 6)
+	{
+		for (int i = 0; i < s.size(); ++i)
+		{
+			if (s[i] >= 48 && s[i] <= 57)
+			{
+				answer = true;
+			}
+			else
+			{
+				answer = false;
+				break;
+			}
+		}
+	}
+
+	cout << boolalpha << answer;
+
+}
