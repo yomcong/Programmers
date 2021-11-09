3609f36c1ea29ab18e29d5e22558cd4ec94c954f
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-10 01:47:50 +0900

    Create JadenCase.cpp
    프로그래머스 2단계 JadenCase문자열 만들기

---
+++ JadenCase.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+
+using namespace std;
+// 프로그래머스 2단계 jadenCase 문자열 만들기
+
+int main()
+{
+	string s1 = "3people Unfollowed Me";
+	string s2 = "for the last week";
+	string answer = "";
+	bool temp = true;
+
+	for (int i = 0; i < s1.size(); ++i)
+	{
+		if (temp && (s1[i] >= 97 && s1[i] <= 122))
+		{
+			s1[i] -= 32;
+			temp = false;
+		}
+		else if(!temp && (s1[i] >= 65 && s1[i] <= 90))
+		{
+			s1[i] += 32;
+			temp = false;
+		}
+		else
+		{
+			temp = false;
+		}
+
+		if (s1[i] == ' ')
+		{
+			temp = true;
+		}
+	}
+	cout << s1;
+}
