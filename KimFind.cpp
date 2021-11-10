a3810fe3852d11f4fe06e4133f94ec5ba7d976c6
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-11 01:46:53 +0900

    Create KimFind.cpp
    KimFind = 프로그래머스 1단계 김서방 찾기

---
+++ KimFind.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<string> seoul = { {"Jane"}, {"Kim"}};
+	string answer = "";
+	int x = 0;
+
+	for (int i = 0; i < seoul.size(); ++i)
+	{
+		if (seoul[i] == "Kim")
+		{
+			x = i;
+			break;
+		}
+	}
+
+
+
+	answer = "김서방은 ";
+	answer += to_string(x);
+	answer += "에 있다";
+
+
+	cout << answer << endl;
+
+}
