d7aae6c48b8efb38a32043ed1debb9c63a3f7ed0
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-13 22:53:13 +0900

    Create MiddleChar.cpp
    프로그래머스 1단계 가운데 글자 가져오기

---
+++ MiddleChar.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 프로그래머스 1단계 가운데 글자 가져오기
+
+int main()
+{
+	string s = "qwer";
+	string answer = "";
+
+	if (s.size() % 2 == 0)
+	{
+		answer.push_back(s[(s.size() / 2)-1]);
+		answer.push_back(s[s.size() / 2]);
+	}
+	else
+	{
+		answer.push_back(s[s.size() / 2]);
+	}
+
+	cout << answer << endl;
+
+}
