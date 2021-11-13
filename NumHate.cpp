19253336565817d163b168f250c02e3be352262d
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-14 00:50:05 +0900

    Create NumHate.cpp
    프로그래머스 1단계 같은 숫자는 싫어

---
+++ NumHate.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 프로그래머스 1단계 같은 숫자는 싫어
+
+int main()
+{
+	vector<int> arr = { {1}, {1}, {3} , {3}, {0}, {1}, {1} };
+	vector<int> answer = {};
+	answer.push_back(arr[0]);
+	
+	for (int i = 1; i < arr.size(); ++i)
+	{
+		if (answer.back() != arr[i])
+		{
+			answer.push_back(arr[i]);
+		}
+	}
+
+	for (auto elem : answer)
+	{
+		cout << elem << endl;
+	}
+}
