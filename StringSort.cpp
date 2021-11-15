1e38f09dbc45b10ee5659c260ee843e6025f7a4e
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-15 15:17:38 +0900

    Create StringSort.cpp
    프로그래머스 1단계 문자열 내 마음대로 정렬하기

---
+++ StringSort.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 프로그래머스 1단계 내림차순으로 배치하기
+
+bool desc(int a, int b)
+{
+	return a > b;
+}
+
+
+int main()
+{
+	string s = "Zbcdefg";
+
+	sort(s.begin(), s.end(), desc);
+
+	cout << s;
+}
