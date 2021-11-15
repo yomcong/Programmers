03059799be59719574882b81afa9c6d43d0a9de2
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-16 03:25:12 +0900

    Create SumOfTwoIntegers.cpp
    프로그래머스 1단계 두 정수의 합

---
+++ SumOfTwoIntegers.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 프로그래머스 1단계 두 정수 사이의 합
+
+int main()
+{
+	int a = 5;
+	int b = 3;
+	int answer = 0;
+	if (a > b)
+	{
+		for (int i = b; i <= a; ++i)
+		{
+			answer += i;
+		}
+	}
+	else if( a<b)
+	{
+		for (int i = a; i <= b; ++i)
+		{
+			answer += i;
+		}
+	}
+	else
+	{
+		answer = a;
+	}
+
+	cout << answer;
+
+
+}
