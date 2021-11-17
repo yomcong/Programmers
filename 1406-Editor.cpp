06e5aa1686a264bcb9561d3e0414c1cdd0574458
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-18 00:33:43 +0900

    Create 1406-Editor.cpp
    백준 1406 에디터

---
+++ 1406-Editor.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    string N = "dmih";
+    int M = 0;
+
+    //cin >> N;
+    cin >> M;
+
+    list<char> lst(N.begin(), N.end());
+    auto iter = lst.end();
+    for (int i = 0; i < M; ++i)
+    {
+        char a;
+        cin >> a;
+        switch (a)
+        {
+        case 'L':
+            if (iter == lst.begin()) break;
+            --iter;
+            break;
+        case 'D':
+            if (iter == lst.end()) break;
+            ++iter;
+            break;
+        case 'B':
+            if (iter == lst.begin()) break;
+            iter = lst.erase(--iter);
+            break;
+        case 'P':
+            char b;
+            cin >> b;
+            lst.insert(iter, b);
+            break;
+
+        }
+    }
+    for (auto elem : lst)
+    {
+        cout << elem;
+    }
+
+}
