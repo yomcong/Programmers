f44565bc8526d50986b6ed245648a0baa8a65263
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-23 01:27:40 +0900

    Create 1463-ToOne.cpp
    백준 1463 1로만들기

---
+++ 1463-ToOne.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+int sol(int a)
+{
+	if (a <= 1)
+	{
+		return 0;
+	}
+
+	/*if (a % 3 == 0)
+	{
+		return sol(a/3) + 1;
+	}
+	
+	if( a %3 == 1)
+	{
+		return sol(a/3) + 2;
+	}
+
+	if (a % 3 == 2)
+	{
+		return sol(a/3) +3;
+	}*/
+	int s1 = sol(a / 3) + a % 3 + 1;
+	int s2 = sol(a / 2) + a % 2 + 1;
+
+
+	return s1 < s2 ? s1 : s2;
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+
+	int a = 0;
+	cin >> a;
+	cout << sol(a);
+
+
+}
