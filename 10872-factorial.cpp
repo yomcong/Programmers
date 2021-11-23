b5d694e580c16553446c3e5bb145f7c615542d86
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-24 02:33:45 +0900

    Create 10872-factorial.cpp
    백준 10872번 팩토리얼

---
+++ 10872-factorial.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+// F(n) = n * F(n-1)
+// F(0) = 1
+
+int factorial(int n)
+{
+	cout << "factorial : " << n << endl;
+	// 기저 조건 (Base Case)
+	if (n == 0)
+	{
+		return 1;
+	}
+
+	// 재귀 조건 (Recursive Case)
+	return factorial(n - 1) * (n);
+
+
+}
+
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+
+	cout << factorial(n);
+
+
+}
