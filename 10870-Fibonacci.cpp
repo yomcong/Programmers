f2a386f9c6b105ecb2f94a4b7ab41a2c22ef070c
yomcong <89593036+yomcong@users.noreply.github.com>
2021-11-28 03:23:31 +0900

    Create 10870-Fibonacci.cpp
    백준 10870번 피보나치

---
+++ 10870-Fibonacci.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+int Fibonacci(int n)
+{
+	// 기저 조건
+	if (n <= 0)
+	{
+		return 0;
+	}
+	if (n == 1)
+	{
+		return 1;
+	}
+
+	// 재귀 조건
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+
+}
+
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+
+	cout << Fibonacci(n);
+
+}
