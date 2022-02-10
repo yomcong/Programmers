76133054c2dc3b6b40cdd0b7a96bde2412d82a5e
yomcong <89593036+yomcong@users.noreply.github.com>
2022-02-11 02:35:05 +0900

    Create 10773-Zero.cpp
    백준 10773 제로

---
+++ 10773-Zero.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <list>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	stack<int> stk = {};
+	int temp = 0;
+	int K = 0;
+	cin >> K;
+	for (int i = 0; i < K; ++i)
+	{
+		int j = 0;
+		cin >> j;
+
+		if (j == 0)
+		{
+			stk.pop();
+		}
+		else
+		{
+			stk.push(j);
+		}
+	}
+	while (!stk.empty())
+	{
+		temp += stk.top();
+		stk.pop();
+	}
+	cout << temp;
+	
+}
