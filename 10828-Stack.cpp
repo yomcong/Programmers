28c4aa17fc685950df4e47d98fcd4e657aaf2725
yomcong <89593036+yomcong@users.noreply.github.com>
2022-02-13 00:13:25 +0900

    Create 10828-Stack.cpp
    백준 10828 스택

---
+++ 10828-Stack.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <list>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	stack<int> stk = {};
+	int N = 0;
+	cin >> N;
+
+	for (int i = 0; i <= N; ++i)
+	{
+		string s = "";
+		getline(cin, s);
+
+		switch (s[0])
+		{
+		case 'p':
+			if (s.size() == 3)
+			{
+				if (stk.empty())
+				{
+					cout << -1 << "\n";
+				}
+				else
+				{
+					cout << stk.top() << "\n";
+					stk.pop();
+				}
+			}
+			else
+			{
+				s.erase(0, 5);
+				stk.push(stoi(s));
+			}
+			break;
+		case 's':
+			cout << stk.size() << "\n";
+			break;
+		case 'e':
+			cout << stk.empty() << "\n";
+			break;
+		case 't':
+			if (stk.empty())
+			{
+				cout << -1 << "\n";
+			}
+			else
+			{
+				cout << stk.top() << "\n";
+			}
+			break;
+		}
+	}
+}
