d610ba9d7ba00666f207139a689e4d0bc6df384d
yomcong <89593036+yomcong@users.noreply.github.com>
2022-03-03 00:12:34 +0900

    Create 15652-NandM-4.cpp
    백준 n과m 4

---
+++ 15652-NandM-4.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int arr[9];
+bool barr[9];
+
+int n, m;
+void foo(int num)
+{
+	if (num == m)
+	{
+		for (int i = 0; i < m; ++i)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (num == 0)
+		{
+			arr[num] = i;
+			foo(num + 1);
+			arr[num] = 0;
+		}
+		else
+		{
+			if (arr[num - 1] <= i)
+			{
+				arr[num] = i;
+				foo(num + 1);
+				arr[num] = 0;
+			}
+		}
+
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+
+	foo(0);
+
+}
