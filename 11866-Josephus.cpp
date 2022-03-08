68a28ad984e0fca50fea083cad4c1081be4c4605
yomcong <89593036+yomcong@users.noreply.github.com>
2022-03-09 00:21:34 +0900

    Create 11866-Josephus.cpp
    백준 11866 요세푸스 문제

---
+++ 11866-Josephus.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	int N = 0;
+	int K = 0;
+
+	queue<int > que = {};
+	queue<int > que2 = {};
+	cin >> N >> K;
+
+	for (int i = 0; i < N; ++i)
+	{
+		que.push(i + 1);
+	}
+
+	while (!que.empty())
+	{
+		for (int i = 0; i < K - 1; ++i)
+		{
+			que.push(que.front());
+			que.pop();
+		}
+		que2.push(que.front());
+		que.pop();
+	}
+	cout << "<";
+	while (!que2.empty())
+	{
+		cout << que2.front();
+		que2.pop();
+		if (que2.empty())
+			cout << "> ";
+		else
+			cout << ", ";
+	}
+
+}
