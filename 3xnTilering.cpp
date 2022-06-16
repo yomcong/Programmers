fa802025df437b10b963f7f576d5b30d4aaaa1e9
yomcong <89593036+yomcong@users.noreply.github.com>
2022-06-17 01:30:26 +0900

    프로그래머스 2단계 3 x n 타일링

---
+++ 3xnTilering.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cout.tie(nullptr);
+//	cin.tie(nullptr);
+//
+//	vector<int> A = {1,2};
+//	vector<int> B = {3,4};
+//
+//	int answer = 0;
+//
+//	sort(A.begin(), A.end());
+//	sort(B.begin(), B.end());
+//
+//	for (int i = 0; i < A.size(); ++i)
+//	{
+//		answer += (A[i] * B[B.size() - 1 - i]);
+//	}
+//
+//	cout << answer;
+//
+//
+//}
+//
+//
+//
+//
+//
+//
